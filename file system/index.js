18fc3932d2ee00f973acb5c51cf662e95be9d2c0
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-10 15:39:12 +0530

    done

--- file system/index.js
+++ file system/index.js
@@ -17,4 +17,34 @@ fs.readFile('./data.json','utf-8',(err,data)=>
           //file system access asynchronous
 })
 
+fs.readdir('c:/',function(err,data)
+{
+    if(err)
+    {
+        console.log(err);   //reading c directory contents synchronously
+    }
+    else
+    {
+        console.log(data);
+    }
+})
+
+var data = {
+    "name":"bob",
+    "age":22
+}
+
+fs.writeFile('./newFile.json',JSON.stringify(data),(err,data)=>
+{
+    if(!err)
+    {
+        console.log("Written complete");                            //writing the data to json file
+    }
+    else
+    {
+        console.log("Error occured!");
+    }
+    
+})
+
 console.log("this is after") //this is exceuted first because of the asynchronous function readFile
